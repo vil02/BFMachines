c81937a3150da4259c6ccaa6381dc23332574a44
Piotr Idzik <vil02@o2.pl>
2020-12-07 21:32:28 +0100

    new files included

--- cpp/BFMachineLib/BFMachineLib.hpp
+++ cpp/BFMachineLib/BFMachineLib.hpp
@@ -2,5 +2,7 @@
 #define BFMACHINELIB_HPP_INCLUDED
 
 #include "./MemoryTypes/MemoryTypes.hpp"
+#include "./Streams/Streams.hpp"
+#include "./BFMachine/BFMachine.hpp"
 
 #endif // BFMACHINELIB_HPP_INCLUDED
