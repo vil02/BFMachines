f348221a41bcbfbf9bd39c546a85de67cc841014
Piotr Idzik <vil02@o2.pl>
2020-12-11 22:45:08 +0100

    removed BoundedMemory

--- cpp/BFMachineLib/MemoryTypes/MemoryTypes.hpp
+++ cpp/BFMachineLib/MemoryTypes/MemoryTypes.hpp
@@ -4,7 +4,6 @@
 #include "VectorMemory.hpp"
 #include "MapMemory.hpp"
 #include "ArrayMemory.hpp"
-#include "BoundedMemory.hpp"
 #include "ChangeValueFunctions.hpp"
 
 #endif // MEMORYTYPES_HPP_INCLUDED
