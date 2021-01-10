5064089b960fb93f03e8f44c429eb58e44678c8a
Piotr Idzik <vil02@o2.pl>
2021-01-10 15:56:05 +0100

    IOStream added

---
+++ matlab/+BFMachinePac/@IOStream/IOStream.m
@@ -0,0 +1,30 @@
+classdef IOStream
+    properties
+        read_fun;
+        write_fun;
+    end
+    
+    methods
+        function obj = IOStream(varargin)
+            input_parser = inputParser;
+            input_parser.addParameter(...
+                'read_fun', ...
+                @() error('No input stream!'), ...
+                @(s) isa(s, 'function_handle'));
+            input_parser.addParameter(...
+                'write_fun', ...
+                @(~) error('No output stream'), ...
+                @(s) isa(s, 'function_handle'));
+            input_parser.parse(varargin{:});
+            obj.read_fun = input_parser.Results.read_fun;
+            obj.write_fun = input_parser.Results.write_fun;
+        end
+        
+        function res = get(obj)
+            res = obj.read_fun();
+        end
+        function put(obj, in_val)
+            obj.write_fun(in_val); 
+        end
+    end
+end
