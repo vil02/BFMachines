216f29b6d7efe3ce0025655cb5dbce91f3a0431e
Piotr Idzik <vil02@o2.pl>
2021-02-06 16:46:02 +0100

    std::move used in bfm::parser::DataChange

--- cpp/BFMachineLib/BFParser/DataChange.hpp
+++ cpp/BFMachineLib/BFParser/DataChange.hpp
@@ -1,6 +1,8 @@
 #ifndef DATACHANGE_HPP_INCLUDED
 #define DATACHANGE_HPP_INCLUDED
 
+#include <utility>
+
 namespace bfm::parser
 {
     template <typename MemoryChangeType>
@@ -12,7 +14,7 @@ namespace bfm::parser
         using value_type = typename memory_change_type::mapped_type;
         DataChange::position_type total_shift;
         DataChange(const MemoryChangeType in_memory_change, const position_type& in_total_shift) :
-            memory_change(in_memory_change),
+            memory_change(std::move(in_memory_change)),
             total_shift(in_total_shift)
         {}
     };
