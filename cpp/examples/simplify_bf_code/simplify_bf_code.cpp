cd65d950b298ba54322b213af1ebb9e4f26bb6e6
Piotr Idzik <vil02@o2.pl>
2021-02-13 18:38:01 +0100

    simplify_bf_code example added

---
+++ cpp/examples/simplify_bf_code/simplify_bf_code.cpp
@@ -0,0 +1,28 @@
+// this file illustrates how to simplify bf code by using the functions
+// bfm::parser::bf_code_to_operation_seq
+// bfm::bfo::translators::ToBf<>::print
+// the resulting code does not contain "meaningless" operations like "<>" or "+-"
+
+#include "../../BFMachineLib/BFMachineLib.hpp"
+
+#include <iostream>
+#include <string>
+
+template <typename CodeType,
+          typename PositionType = std::int64_t,
+          typename ValueType = std::int64_t>
+[[nodiscard]] auto simplify_bf_code(const CodeType& in_bf_code)
+{
+    const auto bf_seq = bfm::parser::bf_code_to_operation_seq<
+        CodeType, PositionType, ValueType>(in_bf_code);
+    return bfm::bfo::translators::ToBf<>::print(bf_seq);
+}
+int main()
+{
+    std::string example_code = ",>-++-ABC<>,<><[->-++<><]>.";
+    std::cout<<"Input code:\n"
+        <<example_code<<'\n'
+        <<"Simplified code:\n"
+        <<simplify_bf_code(example_code)<<'\n';
+    return 0;
+}
