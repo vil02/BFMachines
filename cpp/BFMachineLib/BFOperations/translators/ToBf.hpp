c217077aa57058dc492f45085077bf6def1a7f7e
Piotr Idzik <vil02@o2.pl>
2021-03-27 21:28:12 +0100

    bf_block, bf_simple_loop, bf_loop changed to private

--- cpp/BFMachineLib/BFOperations/translators/ToBf.hpp
+++ cpp/BFMachineLib/BFOperations/translators/ToBf.hpp
@@ -21,49 +21,47 @@ namespace bfm::bfo::translators::general_to_bf
     template <typename DataChangeType, typename InstructionSet>
     class [[nodiscard]] ToBf
     {
-        public:
-            using bf_block = BFBlock<DataChangeType>;
-            using bf_simple_loop = BFSimpleLoop<DataChangeType>;
-            using bf_loop = BFLoop<DataChangeType>;
-        private:
-            template <typename Value, typename Symbol>
-            [[nodiscard]] static auto repeat_symbol(
-                    const Value& in_val,
-                    const Symbol& negative_symbol, const Symbol& positive_symbol)
-            {
-                const auto cur_symbol = in_val < Value(0) ? negative_symbol : positive_symbol;
-                return std::string(std::size_t(inner::abs(in_val)), cur_symbol);
-            }
+        using bf_block = BFBlock<DataChangeType>;
+        using bf_simple_loop = BFSimpleLoop<DataChangeType>;
+        using bf_loop = BFLoop<DataChangeType>;
+        template <typename Value, typename Symbol>
+        [[nodiscard]] static auto repeat_symbol(
+                const Value& in_val,
+                const Symbol& negative_symbol, const Symbol& positive_symbol)
+        {
+            const auto cur_symbol = in_val < Value(0) ? negative_symbol : positive_symbol;
+            return std::string(std::size_t(inner::abs(in_val)), cur_symbol);
+        }
 
-            template <typename ShiftType>
-            [[nodiscard]] static constexpr auto shift_to_bf(const ShiftType& in_shift)
-            {
-                return repeat_symbol(
-                    in_shift, InstructionSet::move_left, InstructionSet::move_right);
-            }
+        template <typename ShiftType>
+        [[nodiscard]] static constexpr auto shift_to_bf(const ShiftType& in_shift)
+        {
+            return repeat_symbol(
+                in_shift, InstructionSet::move_left, InstructionSet::move_right);
+        }
 
-            template <typename ValueType>
-            [[nodiscard]] static constexpr auto value_change_to_bf(const ValueType& in_value_change)
-            {
-                return repeat_symbol(
-                    in_value_change,
-                    InstructionSet::decrease_value, InstructionSet::increase_value);
-            }
+        template <typename ValueType>
+        [[nodiscard]] static constexpr auto value_change_to_bf(const ValueType& in_value_change)
+        {
+            return repeat_symbol(
+                in_value_change,
+                InstructionSet::decrease_value, InstructionSet::increase_value);
+        }
 
-            [[nodiscard]] static auto data_change_to_bf(const DataChangeType& in_data_change)
+        [[nodiscard]] static auto data_change_to_bf(const DataChangeType& in_data_change)
+        {
+            std::stringstream ss;
+            decltype(in_data_change.total_shift) cur_total_shift = 0;
+            for (const auto& [cur_shift, cur_value_change] : in_data_change.memory_change)
             {
-                std::stringstream ss;
-                decltype(in_data_change.total_shift) cur_total_shift = 0;
-                for (const auto& [cur_shift, cur_value_change] : in_data_change.memory_change)
-                {
-                    const auto shift_to_print = cur_shift-cur_total_shift;
-                    ss<<ToBf::shift_to_bf(shift_to_print)
-                        <<ToBf::value_change_to_bf(cur_value_change);
-                    cur_total_shift += shift_to_print;
-                }
-                ss<<ToBf::shift_to_bf(in_data_change.total_shift-cur_total_shift);
-                return ss.str();
+                const auto shift_to_print = cur_shift-cur_total_shift;
+                ss<<ToBf::shift_to_bf(shift_to_print)
+                    <<ToBf::value_change_to_bf(cur_value_change);
+                cur_total_shift += shift_to_print;
             }
+            ss<<ToBf::shift_to_bf(in_data_change.total_shift-cur_total_shift);
+            return ss.str();
+        }
         public:
             [[nodiscard]] static constexpr auto print(const BFWrite& /*unused*/)
             {
