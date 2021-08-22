e469b1d9bb3ade8107c69f563e7ef81f91c91766
piotr.idzik <vil02@o2.pl>
2021-08-22 13:27:35 +0200

    cppcheck settings improved

--- cpp/cmake/StaticAnalyzers.cmake
+++ cpp/cmake/StaticAnalyzers.cmake
@@ -2,16 +2,25 @@ option(ENABLE_CPPCHECK "Enable static analysis with cppcheck" OFF)
 option(ENABLE_CLANG_TIDY "Enable static analysis with clang-tidy" OFF)
 option(ENABLE_INCLUDE_WHAT_YOU_USE "Enable static analysis with include-what-you-use" OFF)
 
+message( ${CMAKE_SOURCE_DIR} )
+
 if(ENABLE_CPPCHECK)
   find_program(CPPCHECK cppcheck)
   if(CPPCHECK)
-    set(CMAKE_CXX_CPPCHECK ${CPPCHECK} --suppress=missingIncludeSystem --enable=all,warning,style,performance,portability,unusedFunction
-                           --inline-suppr --inconclusive
-                           -i ${CMAKE_SOURCE_DIR}/BFMachineLib
-                           )
+    set(CMAKE_CXX_CPPCHECK ${CPPCHECK}
+    --suppress=missingIncludeSystem
+    --enable=all
+    --addon=threadsafety
+    --addon=y2038
+    --addon=cert
+    --inconclusive
+    --force
+    --error-exitcode=1
+    -I ${CMAKE_SOURCE_DIR}/BFMachineLib )
   else()
     message(SEND_ERROR "cppcheck requested but executable not found")
   endif()
+  message(${CMAKE_CXX_CPPCHECK})
 endif()
 
 if(ENABLE_CLANG_TIDY)
